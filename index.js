c019ccc730b5119a3834cad1717e7c05fddba863
Werner <wernercastanaza@gmail.com>
2020-11-28 11:24:44 -0500

    clearing out master branch, all main work will be done on different branches in-order not to get mixed up

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const fs = require('fs');
-
-//Syn is for Syncronous version of file reading
-const txtIn = fs.readFileSync('./input.txt', "utf-8");
-console.log(txtIn);
-
-const txtOut = 'This is what is known about the avocado: ${txtIn}. \nCreated on ${Date.now()}';
-fs.writeFileSync('./txtOut.txt', txtOut);
-
-console.log('Data has been written onto the file');
+console.log('Hello, this is the main branch :)');
