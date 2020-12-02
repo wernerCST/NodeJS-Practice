fb590705c84e32d0af7e3ebd81b248aab4488f2a
Werner <wernercastanaza@gmail.com>
2020-12-01 22:40:36 -0500

    Simple routing example

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello, this is the main branch :)');
+const http = require('http');
+
+// Servenr
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the overview');
+    } else if (pathName === '/product') {
+        res.end('Helo from the product');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello world'
+        });
+        res.end('<h1>404 Page not found</h1>');
+    }
+    
+
+
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listne to requests on port 8000');
+});
