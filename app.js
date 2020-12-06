ba1df6147abb81493afdf648445b86a8403b6908
Werner <wernercastanaza@gmail.com>
2020-12-06 00:38:55 -0500

    all crud methods added, update to clean up routs will fallow

--- app.js
+++ app.js
@@ -67,6 +67,34 @@ app.post('/api/v1/tours', (req, res) => {
     });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => { 
+    
+    if(req.params.id * 1 > tours.length) { 
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status: "success",
+        data: {tour: 'updated tour'}
+    });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => { 
+    
+    if(req.params.id * 1 > tours.length) { 
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(204).json({
+        status: "success",
+        data: null
+    });
+});
+
 
 const port = 3000;
 app.listen(port, () => {
