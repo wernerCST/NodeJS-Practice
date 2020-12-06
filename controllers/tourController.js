cce709dc09e2b5cde59e6d4b402d60697397a535
Werner <wernercastanaza@gmail.com>
2020-12-06 13:20:29 -0500

    adding checks of data within the cust-middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,28 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 ); 
 
+/**  Since this middlewaer is only deffined on this router 
+contrller it only works with tours and not with users controller */
+exports.checkID = (req, res, next, val) => {
+    if(val > tours.length) { 
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    next();
+}
+
+exports.checkBody = (req, res, next) => {
+    if(!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'The content of the body is incompleete'
+        });
+    }
+    next();
+}
+
 exports.getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -13,19 +35,8 @@ exports.getAllTours = (req, res) => {
 };
 
 exports.getTourById = (req, res) => {
-    console.log(req.params);
     const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-    
-    // if(id > tours.length) { 
-    if(!tour) { 
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    
-    
+    const tour = tours.find(el => el.id === id);   
     res.status(200).json({
         status: 'success',
         data: {tour}
@@ -33,12 +44,11 @@ exports.getTourById = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-    // console.log(req.body);
     const newID = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({id: newID}, req.body);
     tours.push(newTour);
     // Never ever ever, block the event loop from a call back fundtion.
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         if(err) { res.status(500).send("server error");}
         res.status(201).json({
             status: 'success',
@@ -50,13 +60,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => { 
-    
-    if(req.params.id * 1 > tours.length) { 
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
     res.status(200).json({
         status: "success",
         data: {tour: 'updated tour'}
@@ -64,13 +67,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => { 
-    
-    if(req.params.id * 1 > tours.length) { 
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
     res.status(204).json({
         status: "success",
         data: null
