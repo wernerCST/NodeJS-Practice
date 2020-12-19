a085ccc88d1513dfcc640321f72c647be7165ed7
Werner <wernercastanaza@gmail.com>
2020-12-19 18:40:27 -0500

    mongoose added to project and connection test added to server.js

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
 const app = require('./app');
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(() => console.log('DB connection succesful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
